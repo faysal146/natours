6f3659bca85ccde9cb4fb4151f64cfd1641b543b
Faysal Ahmed <fahadfaysal146@gmail.com>
2019-10-09 23:57:22 +0600

    install mongodb drive mongoose andconnect data base

--- Server/Server.js
+++ Server/Server.js
@@ -1,9 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('../app');
 
 dotenv.config();
-
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(con => {
+    console.log(con);
+  });
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-	console.log('server is running.....');
+  console.log('server is running.....');
 });
